88c8b2dd75b41a27f63d93b4d237b1f05b70a861
Antoine J.-P. Tixier <Tixierae@users.noreply.github.com>
2018-03-29 15:15:04 +0200

    Update C3Hist.R

--- tutorials/materials2/C3/R/C3Hist.R
+++ tutorials/materials2/C3/R/C3Hist.R
@@ -5,16 +5,14 @@
 #' @import htmlwidgets
 #'
 #' @export
-C3Gauge <- function(value, width = NULL, height = NULL) {
+C3Hist <- function(value, width = NULL, height = NULL) {
 
   # forward options using x
-  x = list(
-    data = value
-  )
+  x = value
 
   # create widget
   htmlwidgets::createWidget(
-    name = 'C3Gauge',
+    name = 'C3Hist',
     x,
     width = width,
     height = height,
@@ -22,30 +20,15 @@ C3Gauge <- function(value, width = NULL, height = NULL) {
   )
 }
 
-#' Shiny bindings for C3Gauge
-#'
-#' Output and render functions for using C3Gauge within Shiny
-#' applications and interactive Rmd documents.
-#'
-#' @param outputId output variable to read from
-#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
-#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
-#'   string and have \code{'px'} appended.
-#' @param expr An expression that generates a C3Gauge
-#' @param env The environment in which to evaluate \code{expr}.
-#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
-#'   is useful if you want to save an expression in a variable.
-#'
-#' @name C3Gauge-shiny
-#'
+
 #' @export
-C3GaugeOutput <- function(outputId, width = '100%', height = '400px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'C3Gauge', width, height, package = 'C3')
+C3HistOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'C3Hist', width, height, package = 'C3')
 }
 
 #' @rdname C3Gauge-shiny
 #' @export
-renderC3Gauge <- function(expr, env = parent.frame(), quoted = FALSE) {
+renderC3Hist <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
-  htmlwidgets::shinyRenderWidget(expr, C3GaugeOutput, env, quoted = TRUE)
+  htmlwidgets::shinyRenderWidget(expr, C3HistOutput, env, quoted = TRUE)
 }
