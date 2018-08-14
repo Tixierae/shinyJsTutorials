d6a7014ca576a2970ae3ce8b988f373c8f63e7d1
antoine_tixier <anti5662@lix.polytechnique.fr>
2018-08-14 13:23:27 +0200

    attempt

---
+++ tutorials/materials2/C3/inst/htmlwidgets/mywidget.js
@@ -0,0 +1,74 @@
+HTMLWidgets.widget({
+
+  name: 'mywidget',
+
+  type: 'output',
+
+  factory: function(el, width, height) {
+
+	
+	var chart = null;
+
+    return {
+
+      renderValue: function(x) {
+
+	  if(chart===null){
+		  
+		keys = _.keys(x.dataset); //var mywidgetData =  //{"Attributes":["trunk","shoulder","elbow"],"Counts":[0.1,0.25,0.65]} 
+		  
+        chart = c3.generate({
+
+            bindto: el,
+                data: {
+
+        json: [],//x.my_df,
+                
+                    keys: {
+                          x: "Outcomes",
+
+                        value: keys,
+                    },
+                    types: {
+
+                        Probability: 'bar'
+                    },
+                  },    bar: {
+        width: {
+            ratio: 0.835
+        }
+    },
+                axis: {rotated: true,
+        x: {
+            type: 'category',
+            tick: {
+                rotate: 65,
+                multiline: false
+            },
+            height: 160
+        },
+                },
+
+        });
+
+          el.chart = chart;
+		
+	  }
+	  
+	  el.chart.load({json: x.dataset});
+	  //el.chart.load({ columns: [
+       //     ['Attributes', 'a', 'b', 'c'],
+        //    ['Counts', 0.1,0.2,0.7]
+        //]});
+	  
+		
+      },
+
+      resize: function(width, height) {
+
+        chart.resize({height:height, width:width})
+      }
+
+    };
+  }
+});
