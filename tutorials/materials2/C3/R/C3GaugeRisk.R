fe8024ad5f9aa0ed46379c8c4abb804bc2fa8a9a
antoine_tixier <anti5662@lix.polytechnique.fr>
2018-11-18 14:36:38 +0100

    anonymous

--- tutorials/materials2/C3/R/C3GaugeRisk.R
+++ tutorials/materials2/C3/R/C3GaugeRisk.R
@@ -10,9 +10,9 @@ C3GaugeRisk <- function(value, my_colors, my_max, my_thresholds, width = NULL, h
   # forward options using x
   x = list(
     risk = list(value),
-	my_colors = my_colors, # ['#60B044','#F9F900','#F6C600','#F97600','#FF0000'] for CRC
-	my_max = my_max, # 200 for CRC TODO update in app!!
-	my_thresholds = my_thresholds # [3,8,17,66,100] for CRC
+	my_colors = my_colors, # e.g., ['#60B044','#F9F900','#F6C600','#F97600','#FF0000']
+	my_max = my_max, 
+	my_thresholds = my_thresholds # e.g., [3,8,17,66,100]
   )
 
   # create widget
