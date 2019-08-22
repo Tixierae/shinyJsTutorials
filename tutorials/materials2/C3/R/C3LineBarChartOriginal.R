83b58c3e8380ea83f20e5966c5e823c8cdd967d3
Antoine J.-P. Tixier <Tixierae@users.noreply.github.com>
2019-08-22 18:57:44 +0200

    Update C3LineBarChartOriginal.R

--- tutorials/materials2/C3/R/C3LineBarChartOriginal.R
+++ tutorials/materials2/C3/R/C3LineBarChartOriginal.R
@@ -5,7 +5,7 @@
 #' @import htmlwidgets
 #'
 #' @export
-C3LineBarChart <- function(dataset, colors, width = NULL, height = NULL) {
+C3LineBarChartOriginal <- function(dataset, colors, width = NULL, height = NULL) {
 
   # forward options using x
   x = list(
@@ -15,7 +15,7 @@ C3LineBarChart <- function(dataset, colors, width = NULL, height = NULL) {
 
   # create widget
   htmlwidgets::createWidget(
-    name = 'C3LineBarChart',
+    name = 'C3LineBarChartOriginal',
     x,
     width = width,
     height = height,
@@ -23,30 +23,30 @@ C3LineBarChart <- function(dataset, colors, width = NULL, height = NULL) {
   )
 }
 
-#' Shiny bindings for C3LineBarChart
+#' Shiny bindings for C3LineBarChartOriginal
 #'
-#' Output and render functions for using C3LineBarChart within Shiny
+#' Output and render functions for using C3LineBarChartOriginal within Shiny
 #' applications and interactive Rmd documents.
 #'
 #' @param outputId output variable to read from
 #' @param width,height Must be a valid CSS unit (like \code{'100\%'},
 #'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
 #'   string and have \code{'px'} appended.
-#' @param expr An expression that generates a C3LineBarChart
+#' @param expr An expression that generates a C3LineBarChartOriginal
 #' @param env The environment in which to evaluate \code{expr}.
 #' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
 #'   is useful if you want to save an expression in a variable.
 #'
-#' @name C3LineBarChart-shiny
+#' @name C3LineBarChartOriginal-shiny
 #'
 #' @export
-C3LineBarChartOutput <- function(outputId, width = '100%', height = '400px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'C3LineBarChart', width, height, package = 'C3')
+C3LineBarChartOriginalOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'C3LineBarChartOriginal', width, height, package = 'C3')
 }
 
-#' @rdname C3LineBarChart-shiny
+#' @rdname C3LineBarChartOriginal-shiny
 #' @export
-renderC3LineBarChart <- function(expr, env = parent.frame(), quoted = FALSE) {
+renderC3LineBarChartOriginal <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
-  htmlwidgets::shinyRenderWidget(expr, C3LineBarChartOutput, env, quoted = TRUE)
+  htmlwidgets::shinyRenderWidget(expr, C3LineBarChartOriginalOutput, env, quoted = TRUE)
 }
