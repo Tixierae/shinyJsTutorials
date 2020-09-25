9042dd7b4047bef33763512fd35e8818c7d872ae
antoine_tixier <anti5662@lix.polytechnique.fr>
2020-09-25 17:32:04 +0200

    fix 0 label risk gauge attempt 10

--- tutorials/materials2/C3/R/C3GaugeRisk.R
+++ tutorials/materials2/C3/R/C3GaugeRisk.R
@@ -9,7 +9,7 @@ C3GaugeRisk <- function(value, my_colors, my_max, my_thresholds, width = NULL, h
 
   # forward options using x
   x = list(
-    risk = list(value),
+    risk = list(value=value),
 	my_colors = my_colors, # ['#60B044','#F9F900','#F6C600','#F97600','#FF0000'] for CRC
 	my_max = my_max, # 200 for CRC TODO update in app!!
 	my_thresholds = my_thresholds # [3,8,17,66,100] for CRC
