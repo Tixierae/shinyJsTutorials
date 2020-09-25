ac2f277ee92f82ca8a37292d2a90bb799468179a
antoine_tixier <anti5662@lix.polytechnique.fr>
2020-09-25 16:24:33 +0200

    fix 0 label risk gauge attempt 2

--- tutorials/materials2/C3/inst/htmlwidgets/C3GaugeRisk.js
+++ tutorials/materials2/C3/inst/htmlwidgets/C3GaugeRisk.js
@@ -19,7 +19,7 @@ HTMLWidgets.widget({
           chart = c3.generate({
                 bindto: el,
                 data: {
-                    json: {value: [x.risk]},
+                    json: {'value': [x.risk]},
                     type: 'gauge',
                     onclick:  function (d, element) { Shiny.onInputChange(el.id,d)}
                 },
