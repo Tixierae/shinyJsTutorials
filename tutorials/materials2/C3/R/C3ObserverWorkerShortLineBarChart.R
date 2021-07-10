cf4cc1926d4d89e3a075b5fe043f5fe50b94aafd
Antoine J.-P. Tixier <Tixierae@users.noreply.github.com>
2021-07-10 17:30:52 +0200

    Create C3ObserverWorkerShortLineBarChart.R

---
+++ tutorials/materials2/C3/R/C3ObserverWorkerShortLineBarChart.R
@@ -0,0 +1,52 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+C3ObserverWorkerShortLineBarChart <- function(dataset, colors, width = NULL, height = NULL) {
+
+  # forward options using x
+  x = list(
+    dataset  = dataset,
+    colors   = colors
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'C3ObserverWorkerShortLineBarChart',
+    x,
+    width = width,
+    height = height,
+    package = 'C3'
+  )
+}
+
+#' Shiny bindings for C3ObserverWorkerShortLineBarChart
+#'
+#' Output and render functions for using C3ObserverWorkerShortLineBarChart within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a C3ObserverWorkerShortLineBarChart
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name C3ObserverWorkerShortLineBarChart-shiny
+#'
+#' @export
+C3ObserverWorkerShortLineBarChartOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'C3ObserverWorkerShortLineBarChart', width, height, package = 'C3')
+}
+
+#' @rdname C3ObserverWorkerShortLineBarChart-shiny
+#' @export
+renderC3ObserverWorkerShortLineBarChart <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, C3ObserverWorkerShortLineBarChartOutput, env, quoted = TRUE)
+}
