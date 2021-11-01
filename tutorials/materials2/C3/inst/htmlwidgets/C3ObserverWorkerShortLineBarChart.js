b4d259bdc943c06acb44413e1472d3129e2bc65d
Antoine J.-P. Tixier <Tixierae@users.noreply.github.com>
2021-11-01 10:48:46 +0100

    Update C3ObserverWorkerShortLineBarChart.js

--- tutorials/materials2/C3/inst/htmlwidgets/C3ObserverWorkerShortLineBarChart.js
+++ tutorials/materials2/C3/inst/htmlwidgets/C3ObserverWorkerShortLineBarChart.js
@@ -57,7 +57,7 @@ HTMLWidgets.widget({
 				  
 				  bar: {
         width: {
-            ratio: 0.60 // this makes bar width 50% of length between ticks
+            ratio: 0.65 // this makes bar width 50% of length between ticks
         }
         // or
         //width: 100 // this makes bar width 100px
